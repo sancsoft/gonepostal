44b29de8d461d7d6e9384934581bd6aaea2c55de
Michael Terry <mterry@sancsoft.com>
2018-04-28 19:25:59 -0400

    initial snapshot

---
+++ src/GonePostal/PostalizedAddress.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper;
+using System.IO;
+using RestSharp;
+using Newtonsoft.Json.Linq;
+
+
+namespace GonePostal
+{
+    public class PostalizedAddress
+    {
+        protected Dictionary<string, string> addressDict;
+
+        public PostalizedAddress()
+        {
+            addressDict = new Dictionary<string, string>();
+        }
+
+        public PostalizedAddress(Dictionary<string, string> dict)
+        {
+            addressDict = dict;
+        }
+
+        public PostalizedAddress(string content)
+        {
+            JArray fields = JArray.Parse(content);
+            addressDict = new Dictionary<string, string>();
+            foreach (JObject obj in fields)
+            {
+                string key = "";
+                string value = "";
+                foreach (JProperty singleProp in obj.Properties())
+                {
+                    if (singleProp.Name.ToString() == "label")
+                    {
+                        key = singleProp.Value.ToString();
+                    }
+                    else if (singleProp.Name.ToString() == "value")
+                    {
+                        value = singleProp.Value.ToString();
+                    }
+                    string name = singleProp.Name;
+                }
+                if (!addressDict.ContainsKey(key))
+                {
+                    addressDict.Add(key, value);
+                }
+                else
+                {
+                    Console.WriteLine("Duplicate key = " + key + " with value = " + value);
+                }
+            }
+        }
+
+        protected string Append(string existing, string add)
+        {
+            string result = existing;
+            if (add.Length > 0)
+            {
+                if (existing.Length > 0)
+                {
+                    result = existing + " " + add;
+                }
+                else
+                {
+                    result = add;
+                }
+            }
+            return result;
+        }
+
+        protected string Value(string key, string def = "")
+        {
+            string value = def;
+            if (addressDict.ContainsKey(key))
+            {
+                value = addressDict[key];
+            }
+            return value;
+        }
+
+        public string StreetAddress()
+        {
+            string streetAddress = "";
+            streetAddress = Append(streetAddress, Value("house_number"));
+            streetAddress = Append(streetAddress, Value("road"));
+            return streetAddress;
+        }
+
+        public string City()
+        {
+            return Value("city");
+        }
+        public string State()
+        {
+            return Value("state");
+        }
+        public string PostalCode()
+        {
+            return Value("postcode");
+        }
+
+        public string Country()
+        {
+            return Value("country");
+        }
+
+    }
+}
---
+++ src/GonePostal/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper;
+using System.IO;
+using RestSharp;
+using Newtonsoft.Json.Linq;
+using System.Dynamic;
+
+namespace GonePostal
+{
+    class Program
+    {
+
+        public static void AddProperty(ExpandoObject expando, string propertyName, object propertyValue)
+        {
+            // ExpandoObject supports IDictionary so we can extend it like this
+            var expandoDict = expando as IDictionary<string, object>;
+            if (expandoDict.ContainsKey(propertyName))
+                expandoDict[propertyName] = propertyValue;
+            else
+                expandoDict.Add(propertyName, propertyValue);
+        }
+
+        static void Main(string[] args)
+        {
+            var client = new RestClient("http://aramis.sancsoft.net:8123");
+            TextReader tr = File.OpenText(@"c:/users/mterry/git/gonepostal/files/RWIZ-Quotes-2017-01-01-Present.csv");
+            StreamWriter sw = new System.IO.StreamWriter(@"c:/users/mterry/git/gonepostal/files/RWIZ-Quotes-2017-01-01-Updated.csv",false);
+            var csvr = new CsvReader(tr);
+            csvr.Read();
+            csvr.ReadHeader();
+            var csvw = new CsvWriter(sw);
+            while (csvr.Read())
+            {
+                var address = csvr["quoAddress"];
+                var record = csvr.GetRecord<dynamic>();
+                address = address.Replace("\r", "");
+                address = address.Replace("\n", " ");
+                Console.WriteLine(address);
+                var request = new RestRequest("/parser", Method.POST);
+                string requestData = "{\"query\":\"" + address + "\"}";
+                request.AddParameter("application/json", requestData, ParameterType.RequestBody);
+                IRestResponse response = client.Execute(request);
+
+                string content = response.Content;
+                Console.WriteLine(content);
+                JArray fields = JArray.Parse(content);
+                PostalizedAddress pa = new PostalizedAddress(content);
+                Console.WriteLine("Street Address = " + pa.StreetAddress());
+                AddProperty(record, "quoStreetAddress", pa.StreetAddress());
+                AddProperty(record, "quoCity", pa.City());
+                AddProperty(record, "quoState", pa.State());
+                AddProperty(record, "quoZip", pa.PostalCode());
+                AddProperty(record, "quoCountry", pa.Country());
+                //record.Add("quoStreetAddress", pa.StreetAddress());
+                csvw.WriteRecord(record);
+                csvw.NextRecord();
+            }
+            sw.Close();
+        }
+    }
+}
